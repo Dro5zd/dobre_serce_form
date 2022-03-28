f369c99263c8d903fbdbed144ccf96d9545e1a83
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-03-28 14:14:23 +0600

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,30 @@ function App() {
     return (
         <div className="App">
             <div className={s.container}>
-                <div className={s.formContainer}>'Заповнить форму'</div>
-                <div className={s.typeContainer}>'Види допомоги'</div>
+                <div className={s.formContainer}>
+                    <h2>Заповнить форму</h2>
+                    <button>Физ. особа</button>
+                    <button>Юр. особа</button>
+                    <div className={s.leftInputGroup}>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                    </div>
+                    <div className={s.rightInputGroup}>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                    </div>
+                </div>
+                <div className={s.typeContainer}>
+                    <h2>Види допомоги</h2>
+                    <h4>Ви можете змінити вид допомоги</h4>
+                </div>
+
             </div>
         </div>
     );
