4b569771629e20f850d56bfac62151bc35cccce5
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-03-28 13:44:35 +0600

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.module.css';
+import s from './App.module.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className={s.container}>
+                <div className={s.formContainer}>'Заповнить форму'</div>
+                <div className={s.typeContainer}>'Види допомоги'</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
